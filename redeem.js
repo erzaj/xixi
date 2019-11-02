41a2293d04eb2f274dbfa8f9dcc5ef354df1f7e0
erzaj <erzajullianalk@gmail.com>
2019-11-02 21:13:45 +0800

    Update redeem.js

--- redeem.js
+++ redeem.js
@@ -17,50 +17,6 @@ const bikinunik = length => new Promise((resolve, reject) => {
         resolve(text);
     });
 
-const functionsendotp = (sessionnya) => new Promise((resolve, reject) => {
-    const url = 'https://api.gojekapi.com/v4/customers/login_with_phone'
-    const badan =  {
-        "phone": nomornya
-    }
-    fetch(url, {
-        method: 'POST',
-        headers: { 
-            'X-Session-ID': sessionnya,
-            'X-Platform': 'Android',
-            'X-UniqueId': 'asuudahyaasu',
-            'X-AppVersion': '3.34.1',
-            'X-AppId': 'com.gojek.apn',
-            // D1: 9E:7B:05:A1:39:3E:15:9C:B5:3D:85:E5:0A:6D:9B:3B:61:0F:50:6A:3A:EB:67:35:73:7B:EB:5F:6E:80:B1:2B 
-            'Accept': 'application/json',
-            'X-PhoneModel': 'Xiaomi,Redmi Note 4',
-            'X-PushTokenType': 'FCM',
-            'X-DeviceOS': 'Android,6.0',
-            // 'User-uuid': '289028939',
-            'X-DeviceToken': 'dEThVxynoKw:APA91bGaRm71ebDIFW-UZu4FDnRA-EqYUIVbZEKgFcdjR0yBTNZeQcFjsG1BQ4RYLS1NtaDy45q6GravAZOnRI9aC4bZYpwyocwhjLB2V0vRv5JcoHgrruUPK01OtlCKNGH8_Ti-FA5U',
-            'Authorization': 'Bearer',
-            'Accept-Language': 'en-ID',
-            'X-User-Locale': 'en_ID',
-            'X-Location': '-6.2178388,106.8953128',
-            'X-Location-Accuracy': '4.433',
-            'X-M1': '1:__9de1deadafae46bbbb6703b54a521a40,2:794723806,3:1566304446377-7239558093263557724,4:54335,5:mt6797|1391|10,6:02:00:00:00:00:00,7:".",8:1080x1920,9:passive\,gps\,network,10:1,11:ZkRNSG5FVHNvb0pGe2FiaFpld0ZlZFdUd1FURFFlUQA=',
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Content-Length': '24',
-            'Host': 'api.gojekapi.com',
-            'Connection': 'Keep-Alive',
-            'Accept-Encoding': 'gzip',
-            'User-Agent': 'okhttp/3.12.1'
-        },
-        body: JSON.stringify(badan)
-    })
-    .then(res => res.json())
-    .then(result => {
-        resolve(result) // console.log(result) PENTING
-    })
-    .catch(err => {
-        reject(err) // console.log(err) PENTING
-    })
-
-});
 
 
 const functionredeemvoc = (sessionnya, uniknya, aksestokennya, kodepromonya) => new Promise((resolve, reject) => {
